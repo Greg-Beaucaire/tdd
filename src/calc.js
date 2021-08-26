4a54b10d4e3a089a64569da2a571b69b1f04637e
Beaucaire Grégoire <greg@cybergoldlaptop.home>
2021-08-26 13:58:21 +0200

    byebye la pyramide of doom

--- src/calc.js
+++ src/calc.js
@@ -5,28 +5,34 @@
  * @param {*} operator must be "add" or "mult" or "div"
  * @returns number, float or string
  */
-const calc = (num1, num2, operator) => {
-  if(num1 && num2 && operator){
-    if(['add', 'mult', 'div'].includes(operator)){
-      if((typeof(num1) == 'number' || typeof(num1) == 'float') && (typeof(num2) == 'number' || typeof(num2) == 'float')){
-        switch(operator) {
-          case "add":
-            return parseFloat((num1 + num2).toFixed(10))
-          case "mult":
-            return parseFloat((num1 * num2).toFixed(10))
-          case "div":
-            if(num2 == 0){
-              return "Error!"
-            }
-            return parseFloat((num1 / num2).toFixed(10))
-          default:
-            return "Error!"
-        }
-      }
-    }
+ const calc = (num1, num2, operator) => {
+  // errors checks
+  if(typeof num1 !== 'number' || typeof num2 !== 'number')
+  {
     return "Error!"
   }
-  return "Error!"
+  
+  if (!operator || ['add', 'mult', 'div'].includes(operator) === false)
+  {
+    return "Error!"
+  }
+
+  // start operation
+  switch(operator)
+  {
+  case 'add':
+    return parseFloat((num1 + num2).toFixed(10))
+
+  case 'mult':
+    return parseFloat((num1 * num2).toFixed(10))
+
+  case 'div':
+    if(num2 === 0)
+    {
+      return "Error!"
+    }
+    return parseFloat((num1 / num2).toFixed(10))
+  }
 }
 
 exports.calc = calc;
