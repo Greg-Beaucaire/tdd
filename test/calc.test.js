06ee463a18f8bb9c1ef3b9391d48a82cf1775b66
Beaucaire Grégoire <greg@cybergoldlaptop.home>
2021-08-26 13:33:36 +0200

    Correction des commentaires de test

--- test/calc.test.js
+++ test/calc.test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const {calc} = require('../src/calc');
 
 describe("Calc", () => {
-    it("Should return 'Error in the operator name!' if operator name isn't mult / add / div", () => {
+    it("Should return 'Error!' if operator name isn't mult / add / div", () => {
       expect(calc(69, "tsouintsouin")).to.equal("Error!");
     });
     it("Handles the lack of parameter", () => {
@@ -26,7 +26,7 @@ describe("Calc", () => {
     it("Multiplication | Decimal : Should return 0.27 by multiplying 0.3 by 0.9", () => {
       expect(calc(0.3, 0.9, "mult")).to.equal(0.27);
     });
-    it("Division by 0 | Should return 'Can't divide by 0' if divider is 0", () => {
+    it("Division by 0 | Should return 'Error!' if divider is 0", () => {
       expect(calc(0.27, 0, "div")).to.equal("Error!");
     });
     it("Division | Integer : Should return 3 by dividing 27 by 9", () => {
