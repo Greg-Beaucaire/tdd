7d7ed2d3a2407ff28777534df5557d3ccaa6effe
Beaucaire Grégoire <greg@cybergoldlaptop.home>
2021-08-26 10:00:41 +0200

    Ajout fonctionnalite multiplication

--- src/calc.js
+++ src/calc.js
@@ -2,6 +2,9 @@ const calc = (num1, num2, operator) => {
   if(operator == 'add'){
     return num1 + num2
   }
+  if(operator == 'mult'){
+    return num1 * num2
+  }
 }
 
 exports.calc = calc;
--- test/calc.test.js
+++ test/calc.test.js
@@ -5,4 +5,7 @@ describe("Calc", () => {
     it("Should return 30 by adding 10 to 20", () => {
         expect(calc(10, 20, "add")).to.equal(30);
     });
+    it("Should return 27 by multiplying 3 by 9", () => {
+      expect(calc(3, 9, "mult")).to.equal(27);
+  });
 })
