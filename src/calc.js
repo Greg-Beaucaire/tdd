c3174220af3b5707ab4beec5b6deff98787342d2
Beaucaire Grégoire <greg@cybergoldlaptop.home>
2021-08-26 10:08:56 +0200

    Ajout du test sur l operateur

--- src/calc.js
+++ src/calc.js
@@ -1,10 +1,16 @@
 const calc = (num1, num2, operator) => {
+
   if(operator == 'add'){
     return num1 + num2
   }
   if(operator == 'mult'){
     return num1 * num2
   }
+  if(operator == 'div'){
+    return num1 / num2
+  }
+  return "Error in the operator name!"
+  
 }
 
 exports.calc = calc;
--- test/calc.test.js
+++ test/calc.test.js
@@ -7,5 +7,11 @@ describe("Calc", () => {
     });
     it("Should return 27 by multiplying 3 by 9", () => {
       expect(calc(3, 9, "mult")).to.equal(27);
-  });
+    });
+    it("Should return 3 by dividing 27 by 9", () => {
+      expect(calc(27, 9, "div")).to.equal(3);
+    });
+    it("Should return 'Error in the operator name!' if operator name isn't mult / add / div", () => {
+      expect(calc(69, 420, "tsouintsouin")).to.equal("Error in the operator name!");
+    });
 })
