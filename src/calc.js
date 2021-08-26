29bf6ae0b500a8bf5511127cdde13ac9f06e482b
Beaucaire Grégoire <greg@cybergoldlaptop.home>
2021-08-26 11:17:00 +0200

    prise en compte de la division par 0

--- src/calc.js
+++ src/calc.js
@@ -7,6 +7,9 @@ const calc = (num1, num2, operator) => {
     return parseFloat((num1 * num2).toFixed(10))
   }
   if(operator == 'div'){
+    if(num2 == 0){
+      return "Can't divide by 0"
+    }
     return parseFloat((num1 / num2).toFixed(10))
   }
   if(!num1 || !num2 || !operator){
--- test/calc.test.js
+++ test/calc.test.js
@@ -2,13 +2,16 @@ const expect = require('chai').expect;
 const {calc} = require('../src/calc');
 
 describe("Calc", () => {
-    it("Should return 30 by adding 10 to 20", () => {
+    it("Should return 0.9 by adding 0.3 to 0.6", () => {
         expect(calc(0.3, 0.6, "add")).to.equal(0.9);
     });
-    it("Should return 27 by multiplying 3 by 9", () => {
+    it("Should return 0.27 by multiplying 0.3 by 0.9", () => {
       expect(calc(0.3, 0.9, "mult")).to.equal(0.27);
     });
-    it("Should return 3 by dividing 27 by 9", () => {
+    it("Should return 'Can't divide by 0' if divider is 0", () => {
+      expect(calc(0.27, 0, "div")).to.equal("Can't divide by 0");
+    });
+    it("Should return 0.3 by dividing 0.27 by 0.9", () => {
       expect(calc(0.27, 0.9, "div")).to.equal(0.3);
     });
     it("Should return 'Error in the operator name!' if operator name isn't mult / add / div", () => {
